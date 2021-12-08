5b9100846f9d9ddcb2c4f76c1ff7bd9176904e5d
Bohdan Khrapak <bodja1999@gmail.com>
2021-12-08 03:48:24 +0200

    Added solution for day 7 part 2

--- 7.py
+++ 7.py
@@ -1,9 +1,15 @@
 from Utils import read_data_as_numbers
 
 positions = read_data_as_numbers(7)
-min_result = None
+min_distance = None
+min_fuel = None
 for i in range(max(positions) + 1):
-    result = sum([abs(position - i) for position in positions])
-    if min_result is None or result < min_result:
-        min_result = result
-print(min_result)
+    distances = [abs(position - i) for position in positions]
+    distance = sum(distances)
+    fuel = sum([(x * (x + 1)) // 2 for x in distances])
+    if min_distance is None or distance < min_distance:
+        min_distance = distance
+    if min_fuel is None or fuel < min_fuel:
+        min_fuel = fuel
+print(min_distance)
+print(min_fuel)
